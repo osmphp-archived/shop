e02e350ee5a0519e78eb663da3cce797a4d6f6de
Vladislav Osmianskij <vlaosm@gmail.com>
2020-04-24 05:54:21 +0300

    fix: using new short layer syntax

--- src/App/backend/layers/page.php
+++ src/App/backend/layers/page.php
@@ -4,24 +4,20 @@ use Osm\Ui\MenuBars\Views\Header;
 use Osm\Ui\Menus\Items\Type;
 
 return [
-    '#page' => [
-        'views' => [
-            'header' => Header::new([
-                'id' => 'header',
-                'wrap_modifier' => '-secondary',
-                'sort_order' => 10,
-            ]),
-        ],
+    '#page.views' => [
+        'header' => Header::new([
+            'id' => 'header',
+            'wrap_modifier' => '-secondary',
+            'sort_order' => 10,
+        ]),
     ],
 
-    '#header.menu' => [
-        'items' => [
-            'home' => [
-                'type' => Type::LINK,
-                'title' => osm_t("Home"),
-                'url' => osm_url('GET /'),
-                'sort_order' => 0,
-            ],
+    '#header.menu.items' => [
+        'home' => [
+            'type' => Type::LINK,
+            'title' => osm_t("Home"),
+            'url' => osm_url('GET /'),
+            'sort_order' => 0,
         ],
     ],
 ];
