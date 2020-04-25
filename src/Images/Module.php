7ef3124af5a3a038b43448960044958f5b3ff781
Vladislav Osmianskij <vlaosm@gmail.com>
2020-04-25 20:44:55 +0300

    minor: new modules and DB schema for image upload

---
+++ src/Images/Module.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Osm\Shop\Images;
+
+use Osm\Core\Modules\BaseModule;
+
+class Module extends BaseModule
+{
+}
---
+++ src/Urls/Module.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Osm\Shop\Urls;
+
+use Osm\Core\Modules\BaseModule;
+
+class Module extends BaseModule
+{
+    public $hard_dependencies = [
+        'Osm_Shop_App',
+    ];
+}
